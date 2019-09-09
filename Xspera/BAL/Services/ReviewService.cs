3612232f1efbbc358aa79bdbd828f4654f8b9953
RunanNgo <48673260+RunanNgo@users.noreply.github.com>
2019-09-09 20:56:15 +0700

    add transaction for addingReview

--- Xspera/BAL/Services/ReviewService.cs
+++ Xspera/BAL/Services/ReviewService.cs
@@ -24,27 +24,47 @@ namespace Xspera.BAL.Services
 
         public Dictionary<bool, string> AddingReview(ReviewRequest reviewRequest)
         {
-            var result = new Dictionary<bool, string>();
-            var reviewDao = this._repository.GetDao<Review>();
-            var userDao = this._repository.GetDao<User>();
-            var productDao = this._repository.GetDao<Product>();
-            var checkUser = userDao.Find(x => x.Type == (int)UserType.Customer).FirstOrDefault();
-            var checkProd = productDao.Find(x => x.Id == reviewRequest.ProductId).FirstOrDefault();
-            if (checkUser == null || checkProd == null || checkUser.Type == (int)UserType.Merchant)
+            using (var trans = this._repository.BeginTransaction())
             {
-                result.Add(false, "User or Product not exist. \n Please try again later");
-                return result;
+                var result = new Dictionary<bool, string>();
+                try
+                {                 
+                    var reviewDao = this._repository.GetDao<Review>();
+                    var userDao = this._repository.GetDao<User>();
+                    var productDao = this._repository.GetDao<Product>();
+                    var checkUser = userDao.Find(x => x.Type == (int)UserType.Customer).FirstOrDefault();
+                    if (checkUser == null || checkUser.Type == (int)UserType.Merchant)
+                    {
+                        result.Add(false, "User not exist or do not have permission. \n Please try again later");
+                        return result;
+                    }
+                    var checkProd = productDao.Find(x => x.Id == reviewRequest.ProductId).FirstOrDefault();
+                    if (checkProd == null)
+                    {
+                        result.Add(false, "Product not exist. \n Please try again later");
+                        return result;
+                    }
+                    var newReview = new Review();
+                    newReview.ProductId = reviewRequest.ProductId;
+                    newReview.UserId = checkUser.Id;
+                    newReview.Rating = reviewRequest.Rating;
+                    newReview.Email = reviewRequest.Email;
+                    newReview.Comment = reviewRequest.Comment;
+                    newReview.DateCreated = DateTime.Now;
+                    reviewDao.Add(newReview);
+                    result.Add(true, "Adding review completed.");
+                    trans.Commit();
+                    return result;
+                }
+                catch (Exception ex)
+                {
+                    trans.Rollback();
+                    result.Add(true, ex.Message);
+                    return result;
+                }
+             
             }
-            var newReview = new Review();
-            newReview.ProductId = reviewRequest.ProductId;
-            newReview.UserId = checkUser.Id;
-            newReview.Rating = reviewRequest.Rating;
-            newReview.Email = reviewRequest.Email;
-            newReview.Comment = reviewRequest.Comment;
-            newReview.DateCreated = DateTime.Now;
-            reviewDao.Add(newReview);
-            result.Add(true, "Adding review completed.");
-            return result;
+       
         }
     }
 }
